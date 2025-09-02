74624c0ebe0d3c8234e24d4abc09d1896bf67956
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2025-09-02 18:02:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+#Romanos:11:36 Porque dele, e por ele, e para ele, são todas as coisas; glória, pois, a ele eternamente. Amém.
+use Core\Controller;
+
+require_once __DIR__.'/configs/config.php';
+require_once __DIR__.'/vendor/autoload.php';
+
+$Router = new Controller();
+$Router->carregar();
+
+
